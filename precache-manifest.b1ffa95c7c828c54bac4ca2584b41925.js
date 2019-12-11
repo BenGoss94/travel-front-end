1147949c943e402918e358a5139364cbe7db8378
BenGoss94 <ben.goss@hotmail.com>
2019-12-11 21:29:38 +0000

    Updates

---
+++ precache-manifest.b1ffa95c7c828c54bac4ca2584b41925.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a6cd638bcf2dcb70452a23728b5d10",
+    "url": "/travel-front-end/index.html"
+  },
+  {
+    "revision": "1e8f955c2e4280ff392b",
+    "url": "/travel-front-end/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3d50d01569f134d2889c",
+    "url": "/travel-front-end/static/js/2.43c19e39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/travel-front-end/static/js/2.43c19e39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e8f955c2e4280ff392b",
+    "url": "/travel-front-end/static/js/main.7e07b562.chunk.js"
+  },
+  {
+    "revision": "3afc57ee2f629e90d376",
+    "url": "/travel-front-end/static/js/runtime-main.bace1c4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travel-front-end/static/media/logo.5d5d9eef.svg"
+  }
+]);
