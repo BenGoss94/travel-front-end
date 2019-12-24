43fe1efff6ca7b5f7aa12580cf1a80af73b9b380
Benjamin Goss <bgoss@BenjamisMacBook.lan>
2019-12-24 22:46:43 +0000

    Updates

---
+++ precache-manifest.16269c7750506e13b08647b4ed8bbece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec224ba636f763f7c78caf1d123fb19",
+    "url": "/travel-front-end/index.html"
+  },
+  {
+    "revision": "b496b43b0555a1cc67c3",
+    "url": "/travel-front-end/static/css/main.0451c2c3.chunk.css"
+  },
+  {
+    "revision": "8c90933ceec365a4fef8",
+    "url": "/travel-front-end/static/js/2.ac4b017a.chunk.js"
+  },
+  {
+    "revision": "fb76c9f8f8182087f9c180bc5fcbff28",
+    "url": "/travel-front-end/static/js/2.ac4b017a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b496b43b0555a1cc67c3",
+    "url": "/travel-front-end/static/js/main.809c8f92.chunk.js"
+  },
+  {
+    "revision": "3afc57ee2f629e90d376",
+    "url": "/travel-front-end/static/js/runtime-main.bace1c4e.js"
+  }
+]);
